820d669e0884b65e2f16154a96ae5397fe3bd112
Jonathan Jefferies <jonathan.jefferies@supergroup.co.uk>
2013-05-07 10:22:51 +0100

    Added RawFTP class.

---
+++ src/jjok/FTP/RawFTP.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace jjok\FTP;
+
+/**
+ * 
+ * @author Jonathan Jefferies
+ * @version 0.1.0
+ */
+class RawFTP {
+
+	/**
+	 * The FTP connection.
+	 * @var resource
+	 */
+	protected $connection;
+	
+	/**
+	 * Close the connection automatically when instance is destroyed.
+	 */
+	public function __destruct() {
+		if(is_resource($this->connection)) {
+			$this->close();
+		}
+	}
+	
+	/**
+	 * Opens an FTP connection.
+	 * @param string $server
+	 * @param number $port
+	 * @param number $timeout
+	 * @return boolean
+	 */
+	public function connect($server, $port = 21, $timeout = 90) {
+		$this->connection = ftp_connect($server, $port, $timeout);
+	
+		return $this->connection !== false;
+	}
+
+	/**
+	 * Closes the current connection.
+	 * @return boolean
+	 */
+	public function close() {
+		return ftp_close($this->connection);
+	}
+	
+	/**
+	 * Send a command to the server.
+	 * @param string $command
+	 * @return array The server response.
+	 */
+	public function raw($command) {
+		return ftp_raw($this->connection, $command);
+	}
+	
+	/**
+	 * 
+	 * @param string $command
+	 * @return boolean
+	 */
+	public function exec($command) {
+		return ftp_exec($this->connection, $command);
+	}
+}
